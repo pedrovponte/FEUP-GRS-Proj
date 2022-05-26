37bb1c7b11aa08147353cd05cf02d0a5fb877b57
Xavier Pisco <x.pisco@gmail.com>
2022-05-26 16:18:47 +0100

    Fixed external host dns

--- new/launchers/externalhost.sh
+++ new/launchers/externalhost.sh
@@ -4,3 +4,8 @@ sudo docker run -d --net public_net --ip 172.31.255.100 \
 --cap-add=NET_ADMIN --name external_host netubuntu
 
 sudo docker exec external_host /bin/bash -c 'ip r a 172.16.123.128/28 via 172.31.255.253'
+sudo docker exec external_host /bin/bash -c \
+'echo "search netlab.fe.up.pt
+nameserver 172.16.123.129
+nameserver 8.8.8.8
+options edns0 trust-ad ndots:0" > /etc/resolv.conf ' 

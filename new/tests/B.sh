9edc423a708b56f9170cc9cb76b5dec01321d335
Xavier Pisco <x.pisco@gmail.com>
2022-06-07 10:20:56 +0100

    Redirect output to /dev/null

--- new/tests/B.sh
+++ new/tests/B.sh
@@ -32,15 +32,21 @@ $CONNECTION client1 google.com; assertTrue
 
 $CONNECTION loadBalancer 10.0.1.3; assertTrue
 $CONNECTION loadBalancer 10.0.2.101; assertTrue
+$CONNECTION loadBalancer 1.1.1.1; assertTrue
+$CONNECTION loadBalancer google.com; assertTrue
+
+# Stop VPN from external_host
+docker exec external_host /bin/bash -c "pkill -f openvpn"
+
 
 $CONNECTION external_host 10.0.1.3; assertFalse
 $CONNECTION external_host 10.0.2.101; assertFalse
 $CONNECTION external_host 172.16.123.132; assertTrue
 $CONNECTION external_host mail.myorg.net; assertTrue
 
-# Connect external host to VPN
+# Connect external_host to VPN
 docker cp ~/usr1.ovpn external_host:/etc/openvpn/usr1.ovpn
-docker exec external_host /bin/bash -c "openvpn --config /etc/openvpn/usr1.ovpn --askpass <(echo '12345')" &
+docker exec external_host /bin/bash -c "openvpn --config /etc/openvpn/usr1.ovpn --askpass <(echo '12345')" > /dev/null &
 sleep 2
 
 $CONNECTION external_host 10.0.1.3; assertTrue
